45fb629743ae5837131753e0a61846f5b3734271
Allison Westphal <trevorawestphal@gmail.com>
2024-08-19 06:46:46 -0600

    Getting mod working

---
+++ DeepSubmergence/DeepSubmergence.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Winch.Core;
+
+namespace DeepSubmergence {
+    public class DeepSubmergence : MonoBehaviour {
+    
+        public static DeepSubmergence instance;
+        
+        // SHORT TERM GOALS
+        // [/] Get mod loading at all
+        // [x] load a model and put it in scene at player (?)
+        // [x] apply texture and material to the model (?)
+        
+        public void Awake(){
+            instance = this;
+            WinchCore.Log.Debug($"[DeepSubmergence] mod loaded");
+        }
+        
+        void Update(){
+            // Nothing yet
+        }
+    }
+}
---
+++ DeepSubmergence/Loader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DeepSubmergence {
+    public class Loader {
+
+        // This method is run by Winch to initialize your mod
+        public static void Initialize()
+        {
+            var gameObject = new GameObject(nameof(DeepSubmergence));
+            gameObject.AddComponent<DeepSubmergence>();
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+}
